d699e91f8298cb50dad8cd0dbd135bb0785b9be9
Philip Bankier <pbankier@miso3.com>
2018-08-11 07:53:30 -0400

    Changed from ExpressJS to Angular 6

---
+++ app/e2e/src/home/home-page.e2e-spec.ts
@@ -0,0 +1,14 @@
+import {AppConfig} from 'src/app/config/app.config';
+import {HomePage} from './home-page';
+
+describe('Home page', function () {
+  let page;
+
+  beforeEach(() => {
+    page = new HomePage();
+  });
+
+  it('should contains TBD content', () => {
+    HomePage.navigateTo();
+  });
+});
